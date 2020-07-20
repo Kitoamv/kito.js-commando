1657977f488a735d32d0ecdace9a28d2262a0704
Kitoamv <49178238+Kitoamv@users.noreply.github.com>
2020-07-20 19:53:43 -0300

    Update user.js

--- src/types/user.js
+++ src/types/user.js
@@ -37,7 +37,7 @@ class UserArgumentType extends ArgumentType {
 			`${disambiguation(
 				members.map(mem => `${escapeMarkdown(mem.user.username)}#${mem.user.discriminator}`), 'users', null
 			)}\n` :
-			'Multiple users found. Please be more specific.';
+			'Vários usuários encontrados. Por favor seja mais específico.';
 	}
 
 	parse(value, msg) {
